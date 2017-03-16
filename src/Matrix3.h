c39fda15a66e1b9a25812df3c8c75a8338de5a95
Sigma0001 <rodrigo.carbajal@saei.mx>
2017-03-16 11:50:59 -0500

    Add files via upload

---
+++ src/Matrix3.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Vec3.h"
+#include <vector>
+
+class Matrix3
+{
+public:
+	float m[3][3];
+
+	Matrix3();
+	Matrix3(const float val[3][3]);
+	Vec3& operator* (const Vec3& v);
+
+	//Matrix3& operator* (const Matrix3& v);
+
+	static Matrix3 rotate(const float a);
+	
+	static Matrix3 mult(const Matrix3& v, const Matrix3& p);
+
+	static Matrix3 Identity();
+
+	static Matrix3 Translate(const Vec3 pos);
+
+	static Matrix3 centro();
+
+};
---
+++ src/ofApp.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "PuntoMedio.h"
+#include "ofMain.h"
+#include "PutPixel.h"
+#include "Vec3.h"
+#include <vector>
+#include "Matrix3.h"
+
+
+class ofApp : public ofBaseApp{
+
+	public:
+		
+		PuntoMedio line;
+		Vec3 mid;
+		
+
+		std::vector<Vec3> vect;
+		std::vector<Vec3> rot;
+
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		void SGasket(const Vec3&a, const Vec3&b, const Vec3&c, const int& level);
+
+		ofVec2f cp;
+		float cX, cY, ang;
+		Vec3 T1,T2,T3,C,mC, Ce;
+		int level;
+		void moveTo(const int x, const int y);
+		void lineTo(const int x, const int y);
+		void poligon(const float cX, const float cY, const int radius, const int sides);
+
+		void triangle(const Vec3 & a, const Vec3 & b, const Vec3 & c);
+	
+};
